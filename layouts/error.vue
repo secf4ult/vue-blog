b26442316c0b626e4b064448e3e8c69ae7f91084
secf4ult <secf4ult@outlook.com>
2020-02-14 16:49:59 +0800

    temp

---
+++ layouts/error.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <h1>Page not find</h1>
+    <h1>An error occured</h1>
+    <nuxt-link to="/">Home page</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error'],
+  layout: 'blog'
+}
+</script>
+
+<style></style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,7 @@ module.exports = {
   /*
    ** Global CSS
    */
-  css: [],
+  css: ['~/assets/main.css'],
   /*
    ** Plugins to load before mounting the App
    */
